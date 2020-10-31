f21b77d47f41c98e0d5e81253a46afd4b1e2893b
wildlifehexagon <hartline.eric@gmail.com>
2020-10-31 02:46:53 +0000

    feat: set up apollo, mui and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import useApolloClient from "./hooks/useApolloClient"
 
-function App() {
+const theme = createMuiTheme({})
+
+const App = () => {
+  const apolloClient = useApolloClient()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={apolloClient}>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>This is the main App component</div>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
